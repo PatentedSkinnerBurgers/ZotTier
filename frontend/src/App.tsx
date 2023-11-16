414e11bcedc87322dc4c6645c553192addd3dab7
Sean Fong <seancfong@gmail.com>
2023-11-16 01:42:27 +0000

    Added some helpful frontend libraries

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const word: string = "word";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {word}
+      <p className="text-red-600">hey</p>
     </div>
   );
 }
