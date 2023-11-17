355aeab7a41fbae0c3d88b44f987311b9099f71e
Jesse David <114846268+jessed7@users.noreply.github.com>
2023-11-16 20:44:09 -0800

    Added header to Browse.tsx

--- frontend/src/pages/Browse.tsx
+++ frontend/src/pages/Browse.tsx
@@ -5,7 +5,9 @@ type Props = {};
 
 const Browse = (props: Props) => {
   return (
-    <div className="browse-gradient h-screen min-h-[300px] px-10 pt-10"></div>
+    <div className="browse-gradient h-screen min-h-[300px] px-10 pt-10">
+      <h1 className="text-white mt-8 font-urbanist text-5xl px-10">Showing all tier lists</h1>
+    </div>
   );
 };
 
