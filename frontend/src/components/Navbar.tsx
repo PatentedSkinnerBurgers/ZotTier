543cc57a07299c437052b258f9d9527e0316ffc6
Sean Fong <seancfong@gmail.com>
2023-11-17 06:17:02 +0000

    Landing page update

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,9 +5,9 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <nav className="fixed w-full px-8">
-      <div className="flex items-end justify-between w-full px-2 h-16 pt-2 pb-2 border-b-2 border-indigo-500 navbar text-zt-light font-urbanist max-w-[1440px] mx-auto">
-        <Link to="/" className="text-2xl">
+    <nav className="absolute top-0 left-0 w-full px-8">
+      <div className="flex items-end justify-between w-full px-2 h-16 pt-2 pb-2 border-b-2 border-[rgba(250,250,250,0.25)] navbar text-zt-light font-urbanist max-w-[1280px] mx-auto">
+        <Link to="/" className="text-3xl font-semibold">
           ZotTier
         </Link>
         <button className="text-lg">Sign in</button>
--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -7,28 +7,37 @@ type Props = {};
 
 const Landing = (props: Props) => {
   return (
-    <div className="flex flex-1 h-screen min-h-[300px] px-10 pt-10 landing-gradient">
-      <div className="absolute top-0 left-0 w-full h-full p-20 pointer-events-none">
-        <LandingDecor />
+    <div className="landing-gradient h-screen min-h-[300px] px-10 pt-20 flex">
+      <div className="max-w-[1280px] mx-auto flex">
+        <div className="absolute top-0 left-0 w-full h-full p-20 pointer-events-none">
+          <LandingDecor />
+        </div>
+        {/* LEFT */}
+        <div className="relative z-10 flex flex-col items-start justify-center w-1/2 gap-3 text-zt-light">
+          <h1 className="text-5xl font-medium leading-[1.2] font-urbanist">
+            Empowering tier lists with <span className="header-zero">Zero</span>{" "}
+            bias.
+          </h1>
+          <p className="pb-2 text-lg font-rubik text-neutral-500">
+            ZOT your vote with some text that will be written soon to come for
+            Anteaters, by Anteaters.
+          </p>
+          <Link
+            to="browse"
+            className="px-6 py-3 text-lg transition-all duration-500 rounded-lg bg-gradient-to-r from-violet-700 to-fuchsia-800 hover:bg-gradient-to-r hover:from-violet-800 hover:to-fuchsia-900"
+          >
+            Discover What's Trending
+          </Link>
+        </div>
+        {/* RIGHT */}
+        <div className="relative w-1/2 overflow-hidden -left-10">
+          <img
+            src={process.env.PUBLIC_URL + "/images/landing-img.png"}
+            alt=""
+            className="object-contain w-full h-full"
+          />
+        </div>
       </div>
-      {/* LEFT */}
-      <div className="relative z-10 flex flex-col items-start justify-center w-1/2 gap-3 text-zt-light">
-        <h1 className="text-5xl font-medium leading-[1.2] font-urbanist">
-          Empowering tier lists with <span className="header-zero">Zero</span>{" "}
-          bias.
-        </h1>
-        <p className="pb-2 text-lg font-rubik text-neutral-500">
-          ZOT your vote with some text that will be written soon to come for
-          Anteaters, by Anteaters.
-        </p>
-        <Link
-          to="browse"
-          className="px-6 py-3 text-lg transition-all duration-500 rounded-lg bg-gradient-to-r from-violet-700 to-fuchsia-800 hover:bg-gradient-to-r hover:from-violet-800 hover:to-fuchsia-900"
-        >
-          Discover What's Trending
-        </Link>
-      </div>
-      {/* RIGHT */}
     </div>
   );
 };
