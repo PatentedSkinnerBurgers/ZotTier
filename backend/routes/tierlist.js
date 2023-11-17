c82fe963b4be41fbb61b5c57d3d83af39545c9ff
PatentedSkinnerBurgers <147116808+PatentedSkinnerBurgers@users.noreply.github.com>
2023-11-17 04:15:56 -0800

    Tier list back-end now gives tier list name in payload

--- backend/routes/tierlist.js
+++ backend/routes/tierlist.js
@@ -6,9 +6,10 @@ const TierCount = 6;
 tierListView.get('/', async (req, res) => {
     try {
         let sql =
-            "SELECT *\n" +
-            "FROM TierList_Items\n" +
-            "WHERE tierListId = ?;"
+            "SELECT TL.name as tierListName, TLI.name as itemName, TLI.numVotes, TLI.voteSum, TLI.imageUrl\n" +
+            "FROM TierList_Items TLI\n" +
+                "INNER JOIN TierList TL on TLI.tierListId = TL.id\n" +
+            "WHERE TL.id = ?;"
 
         pool.getConnection((err, connection) => {
             if (err) throw err;
