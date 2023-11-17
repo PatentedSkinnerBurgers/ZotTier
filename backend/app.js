6b9bc27c611c784f6c779e46681ab6895bed6c79
PatentedSkinnerBurgers <147116808+PatentedSkinnerBurgers@users.noreply.github.com>
2023-11-17 01:41:46 -0800

    Submit-vote post functionality

--- backend/app.js
+++ backend/app.js
@@ -7,12 +7,15 @@ const port = process.env.PORT || 5000;
 const user = require('./routes/user');
 const tierlist_browse = require('./routes/tierlist-browse');
 const tierlist_view = require("./routes/tierlist")
+const submit_vote = require('./routes/submit-vote');
 
 app.use(cors())
+app.use(express.json());
 
 app.use('/user', user);
 app.use('/tierlist-browse', tierlist_browse);
 app.use('/tierlist', tierlist_view);
+app.use('/submit-vote', submit_vote);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
